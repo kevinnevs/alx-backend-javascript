165bace214aeede2d5ceed759a30dbcf2ba80a8d
Kevin Njuguna <kevinnjuguna2@gmail.com>
2023-09-28 22:14:46 +0300

    Update 0-classroom.js
    Fixing typo spacing

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -2,10 +2,10 @@
  * Represents a class room
  */
 export default class ClassRoom {
-/**
- * Creates a new attribute
- * @param (Number) maxStudentsSize
- */
+  /**
+   * Creates a new attribute
+   * @param (Number) maxStudentsSize
+   */
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
