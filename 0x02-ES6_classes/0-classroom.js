aea5cb0e6e0ead6a4fb08720f0ca993d7f2aa75b
Kevin Njuguna <kevinnjuguna2@gmail.com>
2023-09-29 11:57:53 +0300

    Update 0-classroom.js
    Comment changes.

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,11 +1,11 @@
 /**
- * Represents a class room
+ * Represents a class room.
  */
 export default class ClassRoom {
   /**
    * Creates a new @see {@link ClassRoom}.
    * @param {Number} maxStudentsSize - The maximum number of
-   * students in the class
+   * students in the class.
    */
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
