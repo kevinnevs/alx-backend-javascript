80f6007724c8c8cd5743f5b1b936c004d29b574c
kevinnevs <kevinnjuguna2@gmail.com>
2023-11-21 15:08:40 +0000

    fixing typo

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -8,7 +8,7 @@ app.on('request', (_, res) => {
   const responseText = 'Hello Holberton School!';
 
   res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', 'responseText.length');
+  res.setHeader('Content-Length', responseText.length);
   res.statusCode = 200;
   res.write(Buffer.from(responseText));
 });
